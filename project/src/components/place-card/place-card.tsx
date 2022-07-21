cd10f6af1b5f183c8ea0a05f67999d8a12f13568
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-07-21 05:31:16 +0300

    Сохраняет изменения
    # Conflicts:
    #	project/src/components/app/app.tsx
    #	project/src/components/place-card/place-card.tsx
    #	project/src/components/proposal-list/proposal-list.tsx
    #	project/src/index.tsx
    #	project/src/mocks/reviews.ts
    #	project/src/pages/main-screen/main-screen.tsx
    #	project/src/pages/room-screen/room-screen.tsx

---
+++ project/src/components/place-card/place-card.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import {Offer} from '../../types/offers';
+import {capitalizeFirstLetter, renderPremiumLabel} from '../../tools/tools';
+import Rating from '../rating/rating';
+
+type Props = {
+  offer: Offer;
+  onMouseOver?: () => void;
+  onMouseOut?:() => void;
+}
+
+function PlaceCard({offer, onMouseOver, onMouseOut}: Props): JSX.Element {
+
+  return (
+    <article className="cities__card place-card" onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
+      {offer.isPremium ? renderPremiumLabel() : ''}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200"
+            alt="Place image"
+          />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <span className="place-card__price-text">&#47; night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref={'#icon-bookmark'}></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <Rating rating={offer.rating} />
+        <h2 className="place-card__name">
+          <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
+        </h2>
+        <p className="place-card__type">{capitalizeFirstLetter(offer.type)}</p>
+      </div>
+    </article>
+  );
+}
+
+export default PlaceCard;
